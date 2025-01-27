05cf7476f4f851128c5fd27cf2e98d6901d52ec6
AgustinAntonio <agustin@gapy.io>
2025-01-27 13:23:08 -0600

    implemnet latex

--- src/Functions/ResultDisplay.jsx
+++ src/Functions/ResultDisplay.jsx
@@ -1,12 +1,20 @@
+import React from "react";
+import Latex from "react-latex-next";
+
 export default function ResultDisplay({ result }) {
   return (
     <div className="p-4 w-full max-w-md mx-auto mt-6 bg-white shadow rounded-lg">
       <h2 className="text-xl font-bold text-center text-gray-800">Resultado</h2>
       <p className="text-center text-lg mt-4 text-gray-700">
         {result !== null
-          ? `El resultado de la serie es: ${result.result} \n Fórmula utilizada: ${result.formula}`
+          ? (
+              <>
+                <p>El resultado de la serie es: {result.result}</p>
+                <p>Fórmula utilizada: <Latex>{`$${result.formula}$`}</Latex></p>
+              </>
+            )
           : "Introduce un valor válido."}
       </p>
     </div>
   );
-  }
+}
--- src/Functions/SeriesInfo.jsx
+++ src/Functions/SeriesInfo.jsx
@@ -1,13 +1,18 @@
+import React from "react";
+import Latex from "react-latex-next";
 import calculateSeriesTerm from "./calculateSeriesTerm";
 
 export default function SeriesInfo({ n }) {
   if (!n || n <= 0) return null;
 
   // Usar la función calculateSeriesTerm para obtener los valores
-  const { formula } = calculateSeriesTerm(n);
+  const { result, formula } = calculateSeriesTerm(n);
   const primeValue = parseInt(formula.match(/-3\((\d+)\)/)[1], 10);
   const fibonacciValue = parseInt(formula.match(/- \((\d+)\)/)[1], 10);
   const triangularValue = parseInt(formula.match(/\+ 5\((\d+)\)/)[1], 10);
+
+  // Crear solo el cálculo de la operación, eliminando la redundancia
+  const resultFormula = `-3(${primeValue}) - (${fibonacciValue}) + 5(${triangularValue})`;
 
   return (
     <div className="p-4 w-full max-w-md mx-auto mt-6 bg-white shadow rounded-lg">
@@ -24,6 +29,8 @@ export default function SeriesInfo({ n }) {
         <div>
           <h3 className="text-lg font-semibold text-gray-700">Números Triangulares</h3>
           <p className="text-gray-600">El término {n + 1} de la serie triangular es {triangularValue}.</p>
+        </div>
+        <div>
         </div>
       </div>
     </div>
